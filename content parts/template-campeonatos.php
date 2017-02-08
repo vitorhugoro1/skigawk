f1b53ca4cb50f20b82ffd65afc15ee3ee3e9d26c
Vitor Hugo Rodrigues <vitorhugo.ro10@gmail.com>
2017-02-08 14:52:02 -0200

    btn avançar

--- content parts/template-campeonatos.php
+++ content parts/template-campeonatos.php
@@ -1,53 +1,3 @@
-<?php
-$ordem_apresentar = array(
-  'combate' => array('guardas', 'cassetete', 'semi','submission-adulto', 'submission-infantil', 'shuai', 'kuolight', 'kuoleitai', 'wushu', 'sanda', 'muaythai-a', 'muaythai-p', 'cmma', 'mma' ),
-  'formas'  => array('formastradicionais', 'formasinternas', 'formasolimpicas', 'formaslivres', 'tree')
-);
-
-$arr_sub_adulto = array('adulto', 'senior');
-
-if($fetaria == 'mirim' || $fetaria ==  'infantil' || $fetaria == 'ijuvenil' || $fetaria == 'junior'){
-    $file = get_post_meta($_POST['camp_id'], '_vhr_autorizacao_file_id');
-    $parsed = parse_url( wp_get_attachment_url( $file) );
-    $url    = dirname( $parsed [ 'path' ] ) . '/' . rawurlencode( basename( $parsed[ 'path' ] ) );
-    ?>
-    <p>
-        Autorização paulista para atleta menor de idade:
-
-        <a href="<?php echo $url; ?>" target="_blank"> Autorização para Atleta Menor de Idade</a>
-    </p>
-<?php
-}
-?>
-<p>
-    Regras por estilo (Arquivo para Download de acordo com os estilos disponiveis)
-    <ul>
-        <?php
-        $list = wp_get_post_terms( $_POST['camp_id'], 'categoria', array('fields' => 'all') );
-        foreach($list as $term){
-            $in = get_the_author_meta('insiders', $user->ID);
-            if(empty($in) || ! array_key_exists($_POST['camp_id'], $in)){
-                echo '<li>';
-                echo '<a href="'.get_modalidade_file($term->slug).'">'.$term->name.'</a>';
-                echo '</li>';
-            } else {
-                foreach($in[$_POST['camp_id']] as $k => $i){
-                    if($k == 'categorias'){
-                        if(!array_key_exists($term->slug, $i)){
-                            echo '<li>';
-                            echo '<a href="'.get_modalidade_file($term->slug).'">'.$term->name.'</a>';
-                            echo '</li>';
-                        }
-                    }
-                }
-            }
-        }
-
-        ?>
-    </ul>
-</p>
-
-
 Selecione o estilo que vai participar
 <div id="estilo">
     <ul>
@@ -138,7 +88,55 @@ Selecione o estilo que vai participar
     echo '<b>O valor total será mostrado na página seguinte</b><br>';
     } else { ?>
       <b>Campeonato Gratuito</b><br>
-  <?php    } ?>
+  <?php    }
+$ordem_apresentar = array(
+  'combate' => array('guardas', 'cassetete', 'semi','submission-adulto', 'submission-infantil', 'shuai', 'kuolight', 'kuoleitai', 'wushu', 'sanda', 'muaythai-a', 'muaythai-p', 'cmma', 'mma' ),
+  'formas'  => array('formastradicionais', 'formasinternas', 'formasolimpicas', 'formaslivres', 'tree')
+);
+
+$arr_sub_adulto = array('adulto', 'senior');
+
+if($fetaria == 'mirim' || $fetaria ==  'infantil' || $fetaria == 'ijuvenil' || $fetaria == 'junior'){
+    $file = get_post_meta($_POST['camp_id'], '_vhr_autorizacao_file_id');
+    $parsed = parse_url( wp_get_attachment_url( $file) );
+    $url    = dirname( $parsed [ 'path' ] ) . '/' . rawurlencode( basename( $parsed[ 'path' ] ) );
+    ?>
+    <p>
+        Autorização paulista para atleta menor de idade:
+
+        <a href="<?php echo $url; ?>" target="_blank"> Autorização para Atleta Menor de Idade</a>
+    </p>
+<?php
+}
+?>
+<p>
+    Regras por estilo (Arquivo para Download de acordo com os estilos disponiveis)
+    <ul>
+        <?php
+        $list = wp_get_post_terms( $_POST['camp_id'], 'categoria', array('fields' => 'all') );
+        foreach($list as $term){
+            $in = get_the_author_meta('insiders', $user->ID);
+            if(empty($in) || ! array_key_exists($_POST['camp_id'], $in)){
+                echo '<li>';
+                echo '<a href="'.get_modalidade_file($term->slug).'">'.$term->name.'</a>';
+                echo '</li>';
+            } else {
+                foreach($in[$_POST['camp_id']] as $k => $i){
+                    if($k == 'categorias'){
+                        if(!array_key_exists($term->slug, $i)){
+                            echo '<li>';
+                            echo '<a href="'.get_modalidade_file($term->slug).'">'.$term->name.'</a>';
+                            echo '</li>';
+                        }
+                    }
+                }
+            }
+        }
+
+        ?>
+    </ul>
+</p>
+
 <b>Termo de Responsabilidade</b>
 <iframe id="frame" src="" width="100%" height="400px"></iframe>
 <div>
@@ -147,4 +145,4 @@ Selecione o estilo que vai participar
 <br>
 <input type="hidden" name="user_id" value="<?php echo $user->ID; ?>">
 <input type="hidden" name="camp_id" value="<?php echo $_POST['camp_id']; ?>">
-<input type="submit" class="btn btn-primary fp-button" disabled value="Inscrever-se">
+<input type="submit" class="btn btn-primary fp-button" disabled value="Avançar">
